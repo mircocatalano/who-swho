6666e7fc62f32bf76f0f2dd7625132c0cdd23856
Mirco Catalano <catalanomirco04@gmail.com>
2022-10-13 12:54:12 +0200

    Add files via upload

---
+++ sugmaSite.js
@@ -0,0 +1,37 @@
+function dodofart(){
+    alert("dodofart")
+
+    if(document.getElementById('niggas').value == "nd")
+        alert("Select something or select these nuts");
+
+    else if(document.getElementById('niggas').value == "joe")
+        alert("Joe? Joe Mama");
+
+    else if(document.getElementById('niggas').value == "sugma")
+        alert("Sugma what? Sugma dick");
+
+    else if(document.getElementById('niggas').value == "ligma")
+        alert("Ligma what? Ligma ballz");
+
+    else if(document.getElementById('niggas').value == "choka")
+        alert("Ligma what u asking?  Choka ma cock");
+
+    else if(document.getElementById('niggas').value == "wilma")
+        alert("Wilma dick fits in ur mounth?");
+
+    else if(document.getElementById('niggas').value == "cd")
+        alert("Cd nuts");
+
+    else if(document.getElementById('niggas').value == "sugon")
+        alert("Sugon these nuts");
+
+    else if(document.getElementById('niggas').value == "deez")
+        alert("Deez nuts");
+        
+    else if(document.getElementById('niggas').value == "imagine")
+        alert("Imagine these dragon nuts on yo face");
+
+
+    else if(document.getElementById('niggas').value == "niger")
+        alert("Sus");
+}
